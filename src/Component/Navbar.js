d48c883d5539661c974513c98426e70dd19c80ee
moikrom <mikram@gmail.com>
2023-03-29 15:46:26 +0700

    navbar responsive

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,14 +1,17 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
-
 import { Link, useNavigate } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { getProfile } from "../utils/api";
+
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 // import css navbar
 import styles from "../styles/Navbar.module.css";
@@ -19,7 +22,7 @@ import icon_search from "../assets/images/search.png";
 import icon_chat from "../assets/images/chat.png";
 import icon_profile from "../assets/images/default-img.png";
 
-function Navbar() {
+function Navbars() {
   const [profile, setProfile] = useState("");
   const navigate = useNavigate();
 
@@ -83,96 +86,124 @@ function Navbar() {
 
   return (
     <>
-      <div className={styles["container"]}>
-        <nav className={`nav d-flex justify-content-between py-3 ${styles["py"]}`}>
-          <div className={`${styles["left-nav"]} d-flex py-2`}>
-            <img src={icon_coffee} alt="" className="me-2" widht="27px" height="27px" />
-            <span className="">Coffee Gayoe</span>
+      <Navbar collapseOnSelect expand="lg" bg="white" variant="light">
+        <Container className="d-flex align-items-center justify-content-between gaps">
+          <div className="d-flex justify-content-center align-items-center gap-3">
+            <img src={icon_coffee} alt="" widht="27px" height="27px" />
+            <Navbar.Brand href="#home">Coffee Gayoe </Navbar.Brand>
           </div>
-          <div className={`${styles["center-nav"]} d-flex justify-content-center align-items-center`}>
-            <button className={styles["no-underline"]} onClick={toHome}>
-              Home
-            </button>
-            <button className={styles["no-underline"]} onClick={toProduct}>
-              Product
-            </button>
-            <button className={styles["no-underline"]} onClick={token === null ? toasted : toDetailProduct}>
-              {role === "admin" ? "Orders" : "Your Cart"}
-            </button>
-            <button className={styles["no-underline"]} onClick={token === null ? toasted : toHistory}>
-              {role === "admin" ? "Dashboard" : "History"}
-            </button>
-          </div>
-
-          {token === null ? (
-            <div className={`${styles["right-nav"]} d-flex`}>
-              <Button className={styles["login"]} onClick={toLogin}>
-                Login
-              </Button>
-
-              <Button className={`${styles["sign-up"]}`} onClick={toRegister}>
-                Sign Up
-              </Button>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <div className="d-flex justify-content-center align-items-center ">
+              <Nav className="me-auto">
+                <Nav.Link onClick={toHome}>Home</Nav.Link>
+                <Nav.Link onClick={toProduct}>
+                  Product
+                </Nav.Link>
+                <Nav.Link onClick={token === null ? toasted : toDetailProduct}>
+                  Your Cart
+                </Nav.Link>
+                <Nav.Link onClick={token === null ? toasted : toHistory}>
+                  History
+                </Nav.Link>
+              </Nav>
             </div>
-          ) : (
-            <div className={`${styles["right-nav"]} d-flex`}>
-              {/* <a className="nav-link" href="/">
+            {token === null ? (
+              <div className="d-flex justify-content-center align-items-center gap-3">
+                <Button className={styles["login"]} onClick={toLogin}>
+                  Login
+                </Button>
+                <Button className={`${styles["sign-up"]}`} onClick={toRegister}>
+                  Sign Up
+                </Button>
+              </div>
+            ) : (
+              <div className={`${styles["right-nav"]} d-flex`}>
+                {/* <a className="nav-link" href="/">
                 <img clasName={styles.icon} src={icon_search} alt="" widht="27px" height="27px" />
               </a> */}
-              <a className="nav-link" href="/">
-                <img clasName={styles.icon} src={icon_chat} alt="" widht="27px" height="27px" />
-              </a>
+                <a className="nav-link" href="/">
+                  <img
+                    clasName={styles.icon}
+                    src={icon_chat}
+                    alt=""
+                    widht="27px"
+                    height="27px"
+                  />
+                </a>
 
-              {role === "admin" ? (
-                <div>
-                  <button
-                    type="button"
-                    className={`btn ${styles["btn-size"]} ${styles["btn-3"]}`}
-                    onClick={() => {
-                      handleShowModal();
-                    }}
+                {role === "admin" ? (
+                  <div>
+                    <button
+                      type="button"
+                      className={`btn ${styles["btn-size"]} ${styles["btn-3"]}`}
+                      onClick={() => {
+                        handleShowModal();
+                      }}
+                    >
+                      Logout
+                    </button>
+                  </div>
+                ) : (
+                  <Link
+                    className={styles["no-underlinenavbar"]}
+                    to={role === "user" ? "/profile" : "/"}
                   >
-                    Logout
-                  </button>
-                </div>
-              ) : (
-                <Link className={styles["no-underlinenavbar"]} to={role === "user" ? "/profile" : "/"}>
-                  <img className="rounded-circle" src={profile.image === null ? icon_profile : profile.image} alt="" width="50px" height="50px" />
-                </Link>
-              )}
-            </div>
-          )}
-        </nav>
-        <Modal show={showModal} onHide={handleCloseModal} backdrop="static" keyboard={false}>
-          <Modal.Header closeButton>
-            <Modal.Title>confirmation</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>are you sure you want to log out?</Modal.Body>
-          <Modal.Footer>
-            <Button
-              variant="secondary"
-              className="fw-bold text-bg-secondary text-white"
-              onClick={() => {
-                handleCloseModal();
-                deleteToken();
-                window.scrollTo({
-                  top: 0,
-                  left: 0,
-                  behavior: "smooth",
-                });
-              }}
-            >
-              Yes
-            </Button>
-            <Button variant="success" className="fw-bold text-bg-success text-white" onClick={handleCloseModal}>
-              No
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
+                    <img
+                      className="rounded-circle"
+                      src={
+                        profile.image === null ? icon_profile : profile.image
+                      }
+                      alt=""
+                      width="50px"
+                      height="50px"
+                    />
+                  </Link>
+                )}
+              </div>
+            )}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Modal
+        show={showModal}
+        onHide={handleCloseModal}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>confirmation</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>are you sure you want to log out?</Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            className="fw-bold text-bg-secondary text-white"
+            onClick={() => {
+              handleCloseModal();
+              deleteToken();
+              window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: "smooth",
+              });
+            }}
+          >
+            Yes
+          </Button>
+          <Button
+            variant="success"
+            className="fw-bold text-bg-success text-white"
+            onClick={handleCloseModal}
+          >
+            No
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
       <ToastContainer />
     </>
   );
 }
 
-export default Navbar;
+export default Navbars;
