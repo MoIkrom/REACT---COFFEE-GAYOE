45e502e03f790136edfa5fc1a1de99f6a2275796
MoIkrom <mikram216@gmail.com>
2023-04-12 10:26:16 +0700

    Change Password

--- src/pages/EditPwd.js
+++ src/pages/EditPwd.js
@@ -1,11 +1,14 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useState } from "react";
-import styles from "../styles/Forgot.module.css";
+import "../styles/Forgot.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { TabTitle } from "../utils/General-funct.js";
 
 import { toast, ToastContainer } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Card from "react-bootstrap/Card";
 
 import Footer from "../Component/Footer";
 import Navbar from "../Component/Navbar.js";
@@ -53,7 +56,7 @@ const editPwd = () => {
       })
       .catch((err) => {
         setLoading(false);
-        toast.error("wrong Input", {
+        toast.error(old_password === "" || new_password === "" ? "Password Cannot be Empty" : "Password Not Match", {
           position: toast.POSITION.TOP_CENTER,
           autoClose: 2000,
         });
@@ -63,58 +66,54 @@ const editPwd = () => {
 
   return (
     <>
-      <header className={styles["header"]}>
-        <Navbar />
-      </header>
-      <main>
-        <section>
-          <div className={`${styles["jumbotron"]} ${styles["container-fluid"]}`}>
-            <div className="container">
-              <h1 className="display-4 text-white text-center pt-5 fw-bold">Change your Password</h1>
-            </div>
-            <form className={` r-flex jow dustify-content-center ${styles.form}`}>
-              <div className={` col-md-3 col-lg-3 col-xl-6 mx-auto d-flex align-items-center ${styles.inputatas}`}>
-                <label for="inputEmail"></label>
-                <input type={isPwdShown ? "text" : "password"} className={styles["form-control"]} placeholder="Enter your old Password " onChange={handleOldPwd} />
-                <input type={isPwdShown2 ? "text" : "password"} className={styles["form-control"]} placeholder="Enter your new Password " onChange={handleNewPwd} />
-              </div>
-
-              <div className={`container ${styles["cont-text-here"]}`}>
-                <div className={` d-flex justify-content-center  ${styles.change}`}>
-                  <button className={styles["btn-b"]} onClick={handleChange}>
-                    {loading ? (
-                      <>
-                        <div className={styles["lds-ring"]}>
-                          <div></div>
-                          <div></div>
-                          <div></div>
-                          <div></div>
-                        </div>
-                        <p className={styles["loading-text"]}>Loading . . .</p>
-                      </>
-                    ) : (
-                      " Change Password"
-                    )}
-                  </button>
+      <Navbar />
+      <div className="bg">
+        <div className="container d-flex justify-content-center p-5">
+          <Card className="col-md-8 col-lg-5">
+            <Card.Body>
+              <Form onSubmit={handleChange}>
+                <div className="login d-flex align-items-center justify-content-center mt-4 mb-5">
+                  <u className="change">Change Password</u>
                 </div>
-              </div>
-            </form>
-            <div className={`${styles.conteyes}`}>
-              <img onClick={() => setIsPwdShown(true)} src={eyeslash} alt="/" className={isPwdShown === true ? `${styles.none}` : `${styles.show}`} />
-              <img onClick={() => setIsPwdShown(false)} src={eye} alt="/" className={isPwdShown === false ? `${styles.none}` : `${styles.shows}`} />
-            </div>
-            <div className={`${styles.conteyes2}`}>
-              <img onClick={() => setIsPwdShown2(true)} src={eyeslash} alt="/" className={isPwdShown2 === true ? `${styles.none}` : `${styles.show}`} />
-              <img onClick={() => setIsPwdShown2(false)} src={eye} alt="/" className={isPwdShown2 === false ? `${styles.none}` : `${styles.shows}`} />
-            </div>
-          </div>
-        </section>
-        <ToastContainer />
-      </main>
+                <div className="px-md-4 px-lg-5 ">
+                  <Form.Group className="mb-3 " controlId="formBasicEmail">
+                    <Form.Label className="pops">Old Password </Form.Label>
+                    <Form.Control type={isPwdShown ? "text" : "password"} require placeholder="Enter Your Old Password" onChange={handleOldPwd} />{" "}
+                    <div className="d-flex justify-content-end align-items-center gap-2 mt-1 mb-3 pe-1">
+                      <p className="m-0 potz"> Show Password</p>
+                      <img className="img_potz" onClick={() => setIsPwdShown(!isPwdShown)} src={isPwdShown === true ? eye : eyeslash} alt="/" />
+                    </div>
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label className="pops">New Password</Form.Label>
+                    <Form.Control type={isPwdShown2 ? "text" : "Password"} require placeholder=" Enter Your Password" onChange={handleNewPwd} />
+                    <div className="d-flex justify-content-end align-items-center gap-2 mt-1 mb-3 pe-1">
+                      <p className="m-0 potz"> Show Password</p>
+                      <img className="img_potz" onClick={() => setIsPwdShown2(!isPwdShown2)} src={isPwdShown2 === true ? eye : eyeslash} alt="/" />
+                    </div>
+                  </Form.Group>
+                  <div className="d-flex flex-column gap-3 mt-5 mb-4">
+                    <Button disabled={old_password || new_password === "" ? true : false} variant="warning" type="submit">
+                      {loading === true ? (
+                        <div className="d-flex gap-2 justify-content-center align-items-center">
+                          <div class="spinner-border spinner-border-sm text-dark" role="status"></div>
+                          <div>Loading . . .</div>
+                        </div>
+                      ) : (
+                        <div className="popp">Change Password</div>
+                      )}
+                    </Button>
+                  </div>
+                </div>
+              </Form>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
 
-      <footer>
-        <Footer />
-      </footer>
+      <ToastContainer />
+
+      <Footer />
     </>
   );
 };
